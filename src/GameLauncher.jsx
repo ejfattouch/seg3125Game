6c984f369535cf9e82a7ad797b03a9788426b424
James <Jatti045@uottawa.ca>
2025-06-17 23:54:38 -0400

    Buf fixes.

--- src/GameLauncher.jsx
+++ src/GameLauncher.jsx
@@ -1,139 +1,176 @@
-import React, {useState} from "react";
-import {cn} from "@/lib/utils.js";
+import React, { useState } from "react";
+import { cn } from "@/lib/utils.js";
 
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
-import {Button} from "@/components/ui/button.jsx";
+import { Button } from "@/components/ui/button.jsx";
 
 import Game from "@/components/Game.jsx";
 
-import data1 from "@/data/data1.json"
-import data2 from "@/data/data2.json"
-import data3 from "@/data/data3.json"
+import data1 from "@/data/data1.json";
+import data2 from "@/data/data2.json";
+import data3 from "@/data/data3.json";
 
 const GameLauncher = () => {
-    const [gameStart, setGameStart] = useState(false)
-    const [selectedDifficulty, setSelectedDifficulty] = useState([false, false, false]);
-    const [data_select, setDataSelect] = useState(null);
-    const [charList, setCharList] = useState([]);
-    const [wordList, setWordList] = useState([]);
+  const [gameStart, setGameStart] = useState(false);
+  const [selectedDifficulty, setSelectedDifficulty] = useState([
+    false,
+    false,
+    false,
+  ]);
+  const [data_select, setDataSelect] = useState(null);
+  const [charList, setCharList] = useState([]);
+  const [wordList, setWordList] = useState([]);
 
-    const dataArray = [
-        {
-            data: data1,
-            title: "Novice Seeker",
-            desc: "Perfect for beginners.",
-            hint_bg: "bg-green-300",
-            hint_text_color: "text-green-800"
-        },
-        {
-            data: data2,
-            title: "Expert Seeker",
-            desc: "Moderate challenge with more letter combinations.",
-            hint_bg: "bg-blue-300",
-            hint_text_color: "text-blue-800"
-        },
-        {
-            data: data3,
-            title: "Master Seeker",
-            desc: "Ultimate challenge for word masters.",
-            hint_bg: "bg-purple-300",
-            hint_text_color: "text-purple-800"
-        }]
+  const dataArray = [
+    {
+      data: data1,
+      title: "Novice Seeker",
+      desc: "Perfect for beginners.",
+      hint_bg: "bg-green-300",
+      hint_text_color: "text-green-800",
+    },
+    {
+      data: data2,
+      title: "Expert Seeker",
+      desc: "Moderate challenge with more letter combinations.",
+      hint_bg: "bg-blue-300",
+      hint_text_color: "text-blue-800",
+    },
+    {
+      data: data3,
+      title: "Master Seeker",
+      desc: "Ultimate challenge for word masters.",
+      hint_bg: "bg-purple-300",
+      hint_text_color: "text-purple-800",
+    },
+  ];
 
-    const handleGameSelect = (e, index) => {
-        if (e && e.preventDefault) {
-            e.preventDefault();
-        }
-        setDataSelect(index);
+  const handleGameSelect = (e, index) => {
+    if (e && e.preventDefault) {
+      e.preventDefault();
+    }
+    setDataSelect(index);
 
-        const temp = [false, false, false];
-        temp[index] = true;
+    const temp = [false, false, false];
+    temp[index] = true;
 
-        setSelectedDifficulty(temp);
+    setSelectedDifficulty(temp);
+  };
+
+  const startGame = (e) => {
+    if (e && e.preventDefault) {
+      e.preventDefault();
     }
 
-    const startGame = (e) => {
-        if (e && e.preventDefault) {
-            e.preventDefault();
-        }
+    const data = dataArray[data_select].data;
 
-        const data = dataArray[data_select].data;
+    const charList = data.charList.map((char) => char.toUpperCase());
+    const wordList = data.wordList.map((word) => ({
+      word: word.toUpperCase(),
+      status: "missing",
+    }));
 
-        const charList = data.charList.map(char => char.toUpperCase());
-        const wordList = data.wordList.map(word => ({word: word.toUpperCase(), status: 'missing'}));
+    setCharList(charList);
+    setWordList(wordList);
 
-        setCharList(charList);
-        setWordList(wordList);
+    setGameStart(true);
+  };
 
-        setGameStart(true);
-    }
+  return gameStart ? (
+    <Game setGameStart={setGameStart} charList={charList} wordList={wordList} />
+  ) : (
+    <div className="min-h-screen text-center text-white flex flex-col gap-4 space-y-6 mx-4 py-12 px-4 sm:px-0">
+      <div className="flex flex-col gap-4">
+        <h1 className={"text-4xl font-bold"}>🔍 Word Seeker</h1>
+        <p className={"text-lg max-w-xl mx-auto"}>
+          Challenge your mind and seek hidden words. Choose your difficulty
+          level and start your word-hunting adventure!
+        </p>
+      </div>
+      <div>
+        <h1 className={"text-2xl font-bold"}>Choose Your Challenge</h1>
 
-    return (
-        gameStart ?
-            <Game setGameStart={setGameStart} charList={charList} wordList={wordList}/>
-            :
-            <div className="min-h-screen text-center container text-white flex flex-col gap-4 space-y-6 mx-auto py-12 px-4 sm:px-0">
-                <div className="flex flex-col gap-4">
-                    <h1 className={"text-4xl font-bold"}>🔍 Word Seeker</h1>
-                    <p className={"text-lg max-w-xl mx-auto"}>Challenge your mind and seek hidden words. Choose your difficulty level and start your word-hunting adventure!</p>
-                </div>
-                <div>
-                    <h1 className={"text-2xl font-bold"}>Choose Your Challenge</h1>
-
-                    <div className="grid grid-cols-1 gap-10 px-20 lg:px-0 lg:grid-cols-3 mt-6 mb-10 lg:mb-20">
-                        {dataArray.map((data_set, index) => (
-                            <Card
-                                key={index}
-                                onClick={(e) => {handleGameSelect(e, index)}}
-                                className={cn(selectedDifficulty[index]? "border-primary-purple" : "border-gray-600", "bg-gray-700 border cursor-pointer rounded-xl shadow-md")}
-                            >
-                                <CardHeader className={"select-none"}>
-                                    <CardTitle>{data_set.title}</CardTitle>
-                                    <CardDescription>
-                                        <p className={cn("rounded-full w-fit px-3 mx-auto", data_set.hint_bg, data_set.hint_text_color)}>
-                                            {data_set.data.wordList.length} words to find
-                                        </p>
-                                        <p className={"mt-4 text-md"}>{data_set.desc}</p>
-                                    </CardDescription>
-                                </CardHeader>
-                                <CardContent className={"pb-1"}>
-                                    <h1 className={"font-bold text-md select-none"}>Available Letters:</h1>
-                                    <div className={"flex flex-row gap-2 flex-wrap justify-center mt-3"}>
-                                        {data_set.data.charList.map((char) => (
-                                            <span
-                                                key={char.toUpperCase()}
-                                                className="bg-gray-600 text-gray-100 font-bold text-2xl py-2 px-4 rounded-md"
-                                            >
-                                          {char.toUpperCase()}
-                                        </span>
-                                        ))}
-                                    </div>
-                                    {selectedDifficulty[index] ?
-                                        <div className={"mt-6"}>
-                                            <div className={"h-1 w-full bg-primary-purple"}></div>
-                                        </div>:
-                                        <></>}
-
-                                </CardContent>
-                            </Card>
-                        ))}
-                    </div>
-                    <Button
-                        className={cn( data_select === null ?  "bg-gray-400 cursor-not-allowed": "bg-primary-purple cursor-pointer", "text-2xl py-6 px-10 cursor-pointer mb-10 lg:mb-0")}
-                        disabled={data_select === null}
-                        onClick={(e) => {startGame(e)}}
+        <div className="grid grid-cols-1 gap-10 lg:px-0 lg:grid-cols-3 mt-6 mb-10 lg:mb-20">
+          {dataArray.map((data_set, index) => (
+            <Card
+              key={index}
+              onClick={(e) => {
+                handleGameSelect(e, index);
+              }}
+              className={cn(
+                selectedDifficulty[index]
+                  ? "border-primary-purple"
+                  : "border-gray-600",
+                "bg-gray-700 border cursor-pointer rounded-xl shadow-md"
+              )}
+            >
+              <CardHeader className={"select-none"}>
+                <CardTitle>{data_set.title}</CardTitle>
+                <CardDescription>
+                  <p
+                    className={cn(
+                      "rounded-full w-fit px-3 mx-auto",
+                      data_set.hint_bg,
+                      data_set.hint_text_color
+                    )}
+                  >
+                    {data_set.data.wordList.length} words to find
+                  </p>
+                  <p className={"mt-4 text-md"}>{data_set.desc}</p>
+                </CardDescription>
+              </CardHeader>
+              <CardContent className={"pb-1"}>
+                <h1 className={"font-bold text-md select-none"}>
+                  Available Letters:
+                </h1>
+                <div
+                  className={
+                    "flex flex-row gap-2 flex-wrap justify-center mt-3"
+                  }
+                >
+                  {data_set.data.charList.map((char) => (
+                    <span
+                      key={char.toUpperCase()}
+                      className="bg-gray-600 text-gray-100 font-bold text-2xl py-2 px-4 rounded-md"
                     >
-                        Start Seeking!
-                    </Button>
+                      {char.toUpperCase()}
+                    </span>
+                  ))}
                 </div>
-            </div>
-    )
-}
+                {selectedDifficulty[index] ? (
+                  <div className={"mt-6"}>
+                    <div className={"h-1 w-full bg-primary-purple"}></div>
+                  </div>
+                ) : (
+                  <></>
+                )}
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+        <Button
+          className={cn(
+            data_select === null
+              ? "bg-gray-400 cursor-not-allowed"
+              : "bg-primary-purple cursor-pointer",
+            "text-2xl py-6 px-10 cursor-pointer mb-10 lg:mb-0"
+          )}
+          disabled={data_select === null}
+          onClick={(e) => {
+            startGame(e);
+          }}
+        >
+          Start Seeking!
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default GameLauncher;
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,160 +1,191 @@
-import React, { useState } from 'react'
-import { Input } from './ui/input.jsx'
-import { Progress } from './ui/progress.jsx'
-import { ArrowLeftFromLine } from 'lucide-react'
-import {cn} from "@/lib/utils.js";
+import React, { useEffect, useState } from "react";
+import { Input } from "./ui/input.jsx";
+import { Progress } from "./ui/progress.jsx";
+import { ArrowLeftFromLine } from "lucide-react";
+import { cn } from "@/lib/utils.js";
+import Confetti from "react-confetti";
 
-const Game = ({setGameStart, charList, wordList}) => {
-      const [progressStatus, setProgressStatus] = useState(0)
-      const [wordGuess, setWordGuess] = useState("")
-      const [wordLetters, setWordLetters] = useState(charList)
-      const [words, setWords] = useState(wordList)
-      const [isShaking, setIsShaking] = useState(false);
-      const [isCorrect, setIsCorrect] = useState(false);
+const Game = ({ setGameStart, charList, wordList }) => {
+  const [progressStatus, setProgressStatus] = useState(0);
+  const [wordGuess, setWordGuess] = useState("");
+  const [wordLetters, setWordLetters] = useState(charList);
+  const [words, setWords] = useState(wordList);
+  const [isShaking, setIsShaking] = useState(false);
+  const [isCorrect, setIsCorrect] = useState(false);
 
-      const shakeInput = () => {
-          setIsShaking(true);
-          // Stop shaking after a short delay (e.g., 500ms, matching animation duration)
-          setTimeout(() => {
-              setIsShaking(false);
-              setWordGuess("")
-          }, 500); // Adjust this duration to match your animation
-      }
+  const shakeInput = () => {
+    setIsShaking(true);
+    // Stop shaking after a short delay (e.g., 500ms, matching animation duration)
+    setTimeout(() => {
+      setIsShaking(false);
+      setWordGuess("");
+    }, 500); // Adjust this duration to match your animation
+  };
 
-      const setCorrectInput = () => {
-          setIsCorrect(true);
-          setTimeout(() => {
-              setIsCorrect(false);
-          }, 500)
-      }
+  const setCorrectInput = () => {
+    setIsCorrect(true);
+    setTimeout(() => {
+      setIsCorrect(false);
+    }, 500);
+  };
 
-    const checkWordInListOfWords = () => {
-        if (!wordGuess.trim()) return shakeInput();
-    
-        const found = words.find(
-          (w) => w.word.toLowerCase() === wordGuess.toLowerCase()
-        )
-        if (!found || found.status === 'completed') return shakeInput();
+  const checkWordInListOfWords = () => {
+    if (!wordGuess.trim()) return shakeInput();
 
-        const updated = words.map((w) =>
-          w.word === found.word ? { ...w, status: 'completed' } : w
-        )
-        setWords(updated)
-    
-        const completedCount = updated.filter((w) => w.status === 'completed').length
-        const newProgress = Math.round((completedCount / updated.length) * 100)
-        setProgressStatus(newProgress)
-    
-        setWordGuess("")
-    
-        return setCorrectInput();
-      }
+    const found = words.find(
+      (w) => w.word.toLowerCase() === wordGuess.toLowerCase()
+    );
+    if (!found || found.status === "completed") return shakeInput();
+
+    const updated = words.map((w) =>
+      w.word === found.word ? { ...w, status: "completed" } : w
+    );
+    setWords(updated);
+
+    const completedCount = updated.filter(
+      (w) => w.status === "completed"
+    ).length;
+    const newProgress = Math.round((completedCount / updated.length) * 100);
+    setProgressStatus(newProgress);
+
+    setWordGuess("");
+
+    return setCorrectInput();
+  };
+
+  useEffect(() => {
+    if (progressStatus >= 100) {
+      alert("Congratulations! You've found all the words!");
+      setGameStart(false);
+
+      return;
+    }
+  }, [progressStatus]);
+
   return (
     <div className="min-h-screen relative bg-gray-800 text-gray-200 flex flex-col space-y-6 max-w-5xl mx-auto mb-10 py-12 px-4">
-          <ArrowLeftFromLine onClick={() => setGameStart(false)} className='absolute top-13 left-5 size-10 cursor-pointer' />
-          <h1 className="text-4xl font-bold text-center">Word Seeker</h1>
-          <p className="text-gray-400 text-center">
-            Seek and find all hidden words using the given letters
-          </p>
-    
-          <div className="flex gap-4 justify-center">
-            <div className="bg-green-700/30 text-green-300 font-bold py-2 px-4 rounded-full">
-              Words Found: {words.filter((w) => w.status === 'completed').length}/{words.length}
-            </div>
-            <div className="bg-purple-700/30 text-purple-300 font-bold py-2 px-4 rounded-full">
-                Seeker Score: {
-                words.filter((w) => w.status === 'completed')
-                    .reduce((totalScore, wordObject) => totalScore + (wordObject.word.length * 10), 0)
-            }            </div>
-          </div>
-    
-          <section className="bg-gray-700 border border-gray-600 rounded-xl shadow-md p-6">
-            <h2 className="text-xl font-semibold mb-4 text-center">Letter Collection</h2>
-            <div className="flex gap-2 justify-center">
-              {wordLetters.map((ltr) => (
+      <ArrowLeftFromLine
+        onClick={() => setGameStart(false)}
+        className="absolute top-13 left-5 size-10 cursor-pointer"
+      />
+      <h1 className="text-4xl font-bold text-center">Word Seeker</h1>
+      <p className="text-gray-400 text-center">
+        Seek and find all hidden words using the given letters
+      </p>
+
+      <div className="flex gap-4 justify-center">
+        <div className="bg-green-700/30 text-green-300 font-bold py-2 px-4 rounded-full">
+          Words Found: {words.filter((w) => w.status === "completed").length}/
+          {words.length}
+        </div>
+        <div className="bg-purple-700/30 text-purple-300 font-bold py-2 px-4 rounded-full">
+          Seeker Score:{" "}
+          {words
+            .filter((w) => w.status === "completed")
+            .reduce(
+              (totalScore, wordObject) =>
+                totalScore + wordObject.word.length * 10,
+              0
+            )}{" "}
+        </div>
+      </div>
+
+      <section className="bg-gray-700 border border-gray-600 rounded-xl shadow-md p-6">
+        <h2 className="text-xl font-semibold mb-4 text-center">
+          Letter Collection
+        </h2>
+        <div className="flex gap-2 justify-center">
+          {wordLetters.map((ltr) => (
+            <span
+              key={ltr}
+              className="bg-gray-600 text-gray-100 font-bold text-2xl py-2 px-4 rounded-md"
+            >
+              {ltr}
+            </span>
+          ))}
+        </div>
+      </section>
+
+      <section className="bg-gray-700 border border-gray-600 rounded-xl shadow-md flex flex-col gap-4 p-6">
+        <Input
+          type="text"
+          placeholder="Type your word here..."
+          value={wordGuess}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") checkWordInListOfWords();
+          }}
+          onChange={(e) => setWordGuess(e.target.value.toUpperCase())}
+          className={cn(
+            "bg-gray-600 text-gray-100 border border-gray-500 rounded-md w-full placeholder-gray-400 text-center py-4 px-6 text-xl lg:text-2xl",
+            isShaking ? "animate-shake border-red-500" : "",
+            isCorrect ? "border-green-500" : ""
+          )}
+        />
+        <button
+          onClick={checkWordInListOfWords}
+          className="w-full cursor-pointer text-center py-2 rounded-md font-bold bg-gray-100 text-gray-800 hover:bg-gray-200 transition"
+        >
+          Seek Word
+        </button>
+      </section>
+
+      <section className="bg-gray-700 border border-gray-600 rounded-xl shadow-md p-6 text-center">
+        <h2 className="text-xl font-semibold mb-4">Words Found</h2>
+        {words
+          .sort((a, b) => a.word.localeCompare(b.word))
+          .filter((w) => w.status === "completed").length > 0 ? (
+          <div className="flex flex-wrap justify-center gap-2">
+            {words
+              .filter((w) => w.status === "completed")
+              .map((w, i) => (
                 <span
-                  key={ltr}
-                  className="bg-gray-600 text-gray-100 font-bold text-2xl py-2 px-4 rounded-md"
+                  key={i}
+                  className="bg-green-600 text-gray-100 font-bold py-1 px-3 rounded"
                 >
-                  {ltr}
+                  {w.word}
                 </span>
               ))}
-            </div>
-          </section>
-    
-          <section className="bg-gray-700 border border-gray-600 rounded-xl shadow-md flex flex-col gap-4 p-6">
-            <Input
-              type="text"
-              placeholder="Type your word here..."
-              value={wordGuess}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") checkWordInListOfWords();
-              }}
-              onChange={(e) => setWordGuess(e.target.value.toUpperCase())}
-              className={cn(
-                  "bg-gray-600 text-gray-100 border border-gray-500 rounded-md w-full placeholder-gray-400 text-center py-4 px-6 text-xl lg:text-2xl",
-                        isShaking ? "animate-shake border-red-500" : "",
-                        isCorrect ? "border-green-500" : ""
-                  )}
-            />
-            <button
-              onClick={checkWordInListOfWords}
-              className="w-full cursor-pointer text-center py-2 rounded-md font-bold bg-gray-100 text-gray-800 hover:bg-gray-200 transition"
-            >
-              Seek Word
-            </button>
-          </section>
-    
-          <section className="bg-gray-700 border border-gray-600 rounded-xl shadow-md p-6 text-center">
-            <h2 className="text-xl font-semibold mb-4">Words Found</h2>
-            {words.sort((a, b) => a.word.localeCompare(b.word)).filter((w) => w.status === 'completed').length > 0 ? (
-              <div className="flex flex-wrap justify-center gap-2">
-                {words
-                  .filter((w) => w.status === 'completed')
-                  .map((w, i) => (
-                    <span
-                      key={i}
-                      className="bg-green-600 text-gray-100 font-bold py-1 px-3 rounded"
-                    >
-                      {w.word}
-                    </span>
-                  ))}
-              </div>
-            ) : (
-              <p className="text-gray-400">
-                You haven't found any words yet. Try typing some to see how many you can
-                get!
-              </p>
-            )}
-          </section>
-    
-          <section className="bg-gray-700 border border-gray-600 rounded-xl shadow-md p-6">
-            <h2 className="text-lg font-semibold mb-4">Words to Seek</h2>
-            <div className="grid grid-cols-4 gap-4 font-mono select-none">
-              {words.map((wordObj, i) =>
-                  wordObj.status === 'missing' ? (
-                <div
-                  key={i}
-                  className="bg-gray-600 rounded-md px-6 py-8 flex items-center justify-center"
-                >
-                    {wordObj.word.split("").map((char, i) => (<span className=" border w-6 h-6 text-center mx-1 bg-gray-500">?</span>))}
-                </div>
-              ) : null )}
-            </div>
-          </section>
-    
-          <section className="bg-gray-700 border border-gray-600 rounded-xl shadow-md p-6">
-            <div className="flex justify-between mb-2">
-              <span>Seeking Progress</span>
-              <span>{progressStatus}%</span>
-            </div>
-            <Progress
-              value={progressStatus}
-              className="w-full bg-gray-600 border border-gray-500 rounded-full [&>*]:bg-gray-100"
-            />
-          </section>
-        </div>
-  )
-}
+          </div>
+        ) : (
+          <p className="text-gray-400">
+            You haven't found any words yet. Try typing some to see how many you
+            can get!
+          </p>
+        )}
+      </section>
 
-export default Game
+      <section className="bg-gray-700 border border-gray-600 rounded-xl shadow-md p-6">
+        <h2 className="text-lg font-semibold mb-4">Words to Seek</h2>
+        <div className="grid grid-cols-4 gap-4 font-mono select-none">
+          {words.map((wordObj, i) =>
+            wordObj.status === "missing" ? (
+              <div
+                key={i}
+                className="bg-gray-600 rounded-md px-6 py-8 flex flex-wrap gap-2 lg:gap-0 items-center justify-center"
+              >
+                {wordObj.word.split("").map((char, i) => (
+                  <span className=" border w-6 h-6 text-center mx-1 bg-gray-500">
+                    ?
+                  </span>
+                ))}
+              </div>
+            ) : null
+          )}
+        </div>
+      </section>
+
+      <section className="bg-gray-700 border border-gray-600 rounded-xl shadow-md p-6">
+        <div className="flex justify-between mb-2">
+          <span>Seeking Progress</span>
+          <span>{progressStatus}%</span>
+        </div>
+        <Progress
+          value={progressStatus}
+          className="w-full bg-gray-600 border border-gray-500 rounded-full [&>*]:bg-gray-100"
+        />
+      </section>
+    </div>
+  );
+};
+
+export default Game;
